dea3a8d883690a4c57744c9d332869ddf4cf5d5c
DESKTOP-QL9B2DK\DimASSik <goose9098@gmail.com>
2025-09-02 12:23:44 +0300

    создал картинки труб,добавил методы в конструктор,добавил функционал в метод начала игры,начал работу над генерацией труб

--- game.js
+++ game.js
@@ -2,6 +2,10 @@ class SpacePipesGame {
     constructor() {
         this.playerName='';
         this.currentScreen = 'start';
+        this.gameBoard = [];
+        this.boardSize = 6;
+        this.timeLeft = 120;
+        this.timeInterval = null;
         this.init();
     }
     init(){
@@ -28,6 +32,42 @@ class SpacePipesGame {
     }
     startGame() {
         console.log('Игра началась под именем: ' , this.playerName);
+        this.showScreen('game');
+        document.getElementById('current-player').textContent = this.playerName;
+        this.generateGameBoard();
+
+    }
+    generateGameBoard(){
+        const gameBoard = document.getElementById('game-board');
+        gameBoard.innerHTML = '';
+        gameBoard.style.gridTemplateColumns = `repeat(${this.boardSize}, 1fr)`;
+
+        this.gameBoard = [];
+
+        const pipeTypes = [
+            {
+                type: 'straight',
+                rotations: [
+                    { name: 'horizontal', image: 'straight-h.png'},
+                    { name: 'vertical', image: 'straight-v.png'},
+                ]
+            },
+            {
+                type: 'corner',
+                rotations: [
+                    { name: 'top-right', image: 'corner-tr.png'},
+                    { name: 'right-bottom', image: 'corner-rb.png'},
+                    { name: 'bottom-left', image: 'corner-bl.png' },
+                    { name: 'left-top', image: 'corner-lt.png' }
+                ]
+            },
+            {
+                type: 'cross',
+                rotations: [
+                    { name: 'cross', image: 'cross.png'},
+                ]
+            }
+        ];
     }
 }
 
